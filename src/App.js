ca581a3908667544e30e0292d042d41f0ef66132
youngchang8660 <youngchang8660@gmail.com>
2020-03-12 16:22:53 -0600

    completed

--- src/App.js
+++ src/App.js
@@ -1,17 +1,25 @@
 import React, { Component } from 'react';
+import {HashRouter} from 'react-router-dom'
+import routes from './routes'
+import {Link} from 'react-router-dom'
+import About from './components/About/About'
 
 export default class App extends Component {
   render() {
     return (
-      <div>
-        <nav className='nav'>
-          <div>WestSide University</div> 
-          <div className='link-wrap'>
-              <div className='links'>Home</div>
-              <div className='links'>About</div> 
-          </div>
-        </nav>
-      </div>
+      <HashRouter>
+        <div>
+          <nav className='nav'>
+            <div>WestSide University</div> 
+            <div className='link-wrap'>
+                <Link to='/' className='links'>Home</Link>
+                <Link to='/about' className='links'>About</Link> 
+            </div>
+          </nav>
+          {routes}
+        </div>  
+      </HashRouter>
+      
     )
   }
 }
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,12 +1,21 @@
 import React, { Component } from 'react';
+import {Link} from 'react-router-dom'
+import About from '../About/About'
 
 export default class Home extends Component {
   render() {
     return (
       <div className="box">
-        <button className='btn'>Math 1010</button>
-        <button className='btn'>English 2010</button>
-        <button className='btn'>Biology 2020</button>
+        <About />
+        <Link to='/classlist/MATH1010'>
+          <button className='btn'>Math 1010</button>
+        </Link>
+        <Link to='/classlist/ENG2010'>
+          <button className='btn'>English 2010</button>
+        </Link>
+        <Link to='/classlist/BIO2020'>
+          <button className='btn'>Biology 2020</button>
+        </Link>
       </div>
     );
   }
